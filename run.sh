b0c062d0a8783f9a4cd0947f74cd0bd19fb04ef1
Er Jun Li <er.li@mail.mcgill.ca>
2021-02-05 09:38:07 -0500

    remove .vscode settings, add gitignore and server bash script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+export CUDA_VISIBLE_DEVICES=5
+
+# confirm gpu availalable
+nvidia-smi
+
+# get current time
+time="`date +%Y-%m-%d-%H-%M-%S`"
+
+python train_with_teacher.py $@ > logs/$time.log 2>&1 &
+
+
+
